fdd293fe3a826242b3b7276eaca40c2b74dcfa4e
uberkrupali-ui <uberkrupali@gmail.com>
2026-03-01 02:41:06 +0530

    second commit

--- oneinchsensor/Mproject/views.py
+++ oneinchsensor/Mproject/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+def index(request):
+    return render(request, 'index.html')
+    
+def about(request):
+   return render(request, about.html)
 
-# Create your views here.
+def portfolio(request):
+    return render(request, portfolio.html)
+    
+def contact(request):
+    return render(request, contact.html)
