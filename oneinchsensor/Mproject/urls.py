2919175491a61f4c12a21eb0c671b2b8849e2042
uberkrupali-ui <uberkrupali@gmail.com>
2026-03-01 19:16:21 +0530

    fourth commit

--- oneinchsensor/Mproject/urls.py
+++ oneinchsensor/Mproject/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('portfolio', views.portfolio, name='portfolio'),
     path('contact', views.contact, name='contact'),
 ]
